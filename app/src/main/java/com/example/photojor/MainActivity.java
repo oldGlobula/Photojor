14bb02a169febfa1cf3af6f1743adc3843162a9f
oldGlobula <acidman1337@gmail.com>
2019-12-13 23:40:10 +0300

    new image

--- app/src/main/java/com/example/photojor/MainActivity.java
+++ app/src/main/java/com/example/photojor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.photojor;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, List.class);
+                startActivity(i);
+            }
+        };
+        button.setOnClickListener(listener);
     }
 }
