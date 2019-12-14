abd780237dd97e7f071c94ffda59121d286ccb9c
oldGlobula <acidman1337@gmail.com>
2019-12-14 14:29:19 +0300

    wtf

--- app/src/main/java/com/example/photojor/Utils/ApiService.java
+++ app/src/main/java/com/example/photojor/Utils/ApiService.java
@@ -1,4 +1,15 @@
 package com.example.photojor.Utils;
 
+import com.example.photojor.model.ServIngrResponse;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
 public interface ApiService {
+    @Multipart
+    @POST("/api/test")
+    Call<ServIngrResponse> uploadImage(@Part MultipartBody.Part filePart);
 }
---
+++ app/src/main/java/com/example/photojor/model/ServIngrResponse.java
@@ -0,0 +1,13 @@
+package com.example.photojor.model;
+
+public class ServIngrResponse {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
