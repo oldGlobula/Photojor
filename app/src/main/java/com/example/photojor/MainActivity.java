0f9e1e74b8c925aa15d37124263412d45606c038
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2019-12-14 10:12:30 +0300

    Preparation

--- app/src/main/java/com/example/photojor/MainActivity.java
+++ app/src/main/java/com/example/photojor/MainActivity.java
@@ -7,12 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.photojor.Utils.ApiService;
+import com.example.photojor.Utils.Consts;
+import com.google.gson.GsonBuilder;
+
+import javax.crypto.SecretKey;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Consts.retrofit = new Retrofit.Builder()
+                .baseUrl(Consts.baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Consts.service = Consts.retrofit.create(ApiService.class);
+
+        Consts.gsonBuilder = new GsonBuilder();
+        //Consts.gsonBuilder.registerTypeAdapter(SecretKey.class, new SecretKeyAdapter());
+        Consts.gson = Consts.gsonBuilder.create();
+
         Button button = findViewById(R.id.button);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -22,5 +41,6 @@ public class MainActivity extends Activity {
             }
         };
         button.setOnClickListener(listener);
+
     }
 }
---
+++ app/src/main/java/com/example/photojor/Utils/ApiService.java
@@ -0,0 +1,4 @@
+package com.example.photojor.Utils;
+
+public interface ApiService {
+}
